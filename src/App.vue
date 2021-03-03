2b3b166b24eecf715743711a96675d2133779b21
IshanKBG <jaiswalishan701@gmail.com>
2021-03-03 08:28:42 +0530

    Huh

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,24 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <Header intro="I am Ishan, a Full-Stack Developer"></Header>
 </template>
 
 <script lang="ts">
 import { Options, Vue } from 'vue-class-component';
-import HelloWorld from './components/HelloWorld.vue';
-
+import Header from './components/header.vue';
+//import { auth, db } from './firebase';
+//console.log(auth)
 @Options({
   components: {
-    HelloWorld,
+    Header,
   },
 })
+
 export default class App extends Vue {}
 </script>
-
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style type="text/css" media="all">
+ /* * {
+    box-sizing: border-box;
+    padding: 0;
+    margin: 0;
+  }*/
 </style>
---
+++ src/components/header.vue
@@ -0,0 +1,18 @@
+<template>
+  <header>
+      <p class="intro intro-anim">{{intro}}</p>
+  </header>
+</template>
+<style lang="scss">
+@import '../assets/scss/style.scss'
+</style>
+<script>
+import { Options, Vue } from 'vue-class-component';
+
+@Options({
+  props: {
+   intro: String
+  }
+})
+export default class Header extends Vue {}
+</script>
